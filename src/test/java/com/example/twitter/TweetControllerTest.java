ab9e4bd07fd9a0ff8e5c58c8fcdd39c22e31f3db
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-24 11:57:26 +0100

    Everything is working, need to deploy now

--- src/test/java/com/example/twitter/TweetControllerTest.java
+++ src/test/java/com/example/twitter/TweetControllerTest.java
@@ -11,14 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
 public class TweetControllerTest {
 
     @InjectMocks
--- src/test/java/com/example/twitter/UserServiceTest.java
+++ src/test/java/com/example/twitter/UserServiceTest.java
@@ -98,5 +98,4 @@ public class UserServiceTest {
         // Assert
         assertThat(result).isEqualTo(user);
     }
-
 }
